2cf4c4ab1b384dbf6a26b568ea19cd2a89ebe93a
kimgeonjung <rjs2021@gmail.com>
2024-12-23 11:33:18 +0900

    create test4 project

---
+++ src/main/java/test4/controller/IndexController.java
@@ -0,0 +1,14 @@
+package test4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("message", "타임리프테스트");
+		return "index.html";
+	}
+}
